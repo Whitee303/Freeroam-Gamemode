6c0aee1780259b9eef3c1f6940653f5da05ada41
Whitee <84782812+Whitee303@users.noreply.github.com>
2021-10-29 20:33:42 +0200

    Add files via upload

--- client/index.js
+++ client/index.js
@@ -1,28 +1,29 @@
 /// <reference types="@altv/types-client" />
 /// <reference types="@altv/types-client" />
 /// <reference types="@altv/types-natives" />
+import * as alt from 'alt-client';
+import * as natives from 'natives';
 
-import alt from 'alt-client'
-import native from "natives";
-
-const localPlayer = alt.Player.local.scriptID;
+const localPlayer = alt.Player.local;
 
 
-alt.onServer("event",async Veh => {
-    await new Promise(resolve => {
-        const retry = setInterval(() => {
-             if(Veh?.scriptID !== undefined) {
-                  clearInterval(retry);
-                  return resolve();
-              }
-        }, 50);
-    });
-        native.setPedIntoVehicle(localPlayer,Veh.scriptID,-1);
-        native.setVehicleEngineOn(Veh.scriptID,false,true,true);
-        alt.on('keydown', (key) => {
-            if(key === 0x45) {
-                alt.log("someone");
-                native.setVehicleEngineOn(Veh.scriptID,true,true,true);
-            }
-        });
-});
+alt.onServer('drawNotification', drawNotification);
+
+export function drawNotification(imageName, headerMsg, detailsMsg, message) {
+    natives.beginTextCommandThefeedPost('STRING');
+    natives.addTextComponentSubstringPlayerName(message);
+    natives.endTextCommandThefeedPostMessagetextTu(
+        imageName.toUpperCase(),
+        imageName.toUpperCase(),
+        false,
+        4,
+        headerMsg,
+        detailsMsg,
+        1.0,
+        ''
+    );
+    natives.endTextCommandThefeedPostTicker(false, false);
+}
+
+natives.addBlipForEntity(localPlayer)
+
